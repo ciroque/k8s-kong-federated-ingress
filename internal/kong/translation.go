fbc8ffa39a0721567aa9509dfae67f68ae2e6f19
swagner <swagner@marchex.com>
2019-09-10 13:57:19 -0700

    Adds Addresses to the Translator

--- internal/kong/translation.go
+++ internal/kong/translation.go
@@ -1,7 +1,9 @@
 package kong
 
 import (
+	"fmt"
 	"github.com/ciroque/k8s-kong-federated-ingress/internal/k8s"
+	v1 "k8s.io/api/core/v1"
 	networking "k8s.io/api/networking/v1beta1"
 )
 
@@ -13,12 +15,12 @@ type Translation struct {
 }
 
 func (translation *Translation) IngressToService(ingress *networking.Ingress) (*k8s.Service, error) {
-
 	k8sService := new(k8s.Service)
 	var paths []string
 
 	for _, rule := range ingress.Spec.Rules {
 		for _, path := range rule.HTTP.Paths {
+			//k8sService.Name = ingress.Namespace + "." + path.Backend.ServiceName + ".service"
 			k8sService.Name = path.Backend.ServiceName
 			k8sService.Port = int(path.Backend.ServicePort.IntVal)
 			paths = append(paths, path.Path)
@@ -26,6 +28,16 @@ func (translation *Translation) IngressToService(ingress *networking.Ingress) (*
 	}
 
 	k8sService.Paths = paths
+	k8sService.Addresses = buildAddresses(ingress.Status.LoadBalancer.Ingress)
 
 	return k8sService, nil
+}
+
+func buildAddresses(ingresses []v1.LoadBalancerIngress) []string {
+	var addresses []string
+	for _, ingress := range ingresses {
+		address := fmt.Sprintf("%s:80", ingress.IP)
+		addresses = append(addresses, address)
+	}
+	return addresses
 }
--- internal/kong/translation_test.go
+++ internal/kong/translation_test.go
@@ -2,12 +2,16 @@ package kong
 
 import (
 	"github.com/ciroque/k8s-kong-federated-ingress/internal/k8s"
+	v1 "k8s.io/api/core/v1"
 	networking "k8s.io/api/networking/v1beta1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"testing"
 )
 
 func compareStringArrays(l []string, r []string) bool {
+	if len(l) != len(r) {
+		return false
+	}
 	for i, e := range l {
 		if r[i] != e {
 			return false
@@ -19,12 +23,18 @@ func compareStringArrays(l []string, r []string) bool {
 func servicesMatch(l k8s.Service, r k8s.Service) bool {
 	return l.Name == r.Name &&
 		compareStringArrays(l.Paths, r.Paths) &&
-		l.Port == r.Port /// && compareStringArrays(l.Addresses, r.Addresses)
+		l.Port == r.Port &&
+		compareStringArrays(l.Addresses, r.Addresses)
 }
 
-func TestTranslation_IngressToService_RouteAndService(t *testing.T) {
+func TestTranslation_IngressToService(t *testing.T) {
+	//addresses := []string{}
+	addresses := []string{
+		"10.200.30.400:80",
+		"10.200.30.401:80",
+	}
 	expectedService := k8s.Service{
-		Addresses: nil,
+		Addresses: addresses,
 		Name:      "test-service-name",
 		Paths:     []string{"/apple", "/banana"},
 		Port:      80,
@@ -32,7 +42,21 @@ func TestTranslation_IngressToService_RouteAndService(t *testing.T) {
 
 	translation := new(Translation)
 	ingress := networking.Ingress{
+		Status: networking.IngressStatus{
+			LoadBalancer: v1.LoadBalancerStatus{
+				Ingress: []v1.LoadBalancerIngress{
+					{
+						IP: "10.200.30.400",
+					},
+					{
+						IP: "10.200.30.401",
+					},
+				},
+			},
+		},
 		Spec: networking.IngressSpec{
+			Backend: nil,
+			TLS:     nil,
 			Rules: []networking.IngressRule{
 				{
 					"test-host",
@@ -60,6 +84,7 @@ func TestTranslation_IngressToService_RouteAndService(t *testing.T) {
 			},
 		},
 	}
+	ingress.Namespace = "testing-namespace"
 	actualService, err := translation.IngressToService(&ingress)
 
 	if err != nil {
